402ff6cab286512b6d19ac48383abf0f169e0dba
Junghwan Park <jup014@ucsd.edu>
2022-07-14 14:31:15 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyjitai',
+    version='0.0.0.3',
+    description='PyJITAI',
+    author='jup014',
+    author_email='jup014@ucsd.edu',
+    url='https://cwphs.ucsd.edu',
+    license='MIT',
+    py_modules=['pyjitai'],
+    python_requires='>=3',
+    install_requires=[],
+    packages=['pyjitai']
+)
